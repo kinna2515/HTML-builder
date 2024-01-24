9d6c1841ab55e6afb485f95c8f6437c232b977c6
Inna Barysenka <inna.barysenka@gmail.com>
2024-01-24 12:40:54 +0300

    fix: fix add/delete new file functionality in files folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 const { mkdir } = require('node:fs/promises');
 const copyFile = fs.promises.copyFile;
 
 async function createCopyFolder() {
   const copyFolder = path.join(__dirname, 'files-copy');
+  await fs.promises.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true });
   const copyFolderCreation = await mkdir(copyFolder, { recursive: true });
+  
+
   let folderItem = await fs.promises.readdir(path.join(__dirname, 'files'));
   folderItem.forEach(async (file) => {
     const filePath = path.join(__dirname, 'files', file);
     await copyFile(filePath, path.join(__dirname, 'files-copy', file));
     console.log(file);
   });
-  
   return copyFolderCreation;
 }
 
